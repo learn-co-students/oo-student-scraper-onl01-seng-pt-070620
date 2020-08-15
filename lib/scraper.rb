5b74005cb88ea60bc389deaab6c286cf584a2027
Jill Short <jillian.b.short@gmail.com>
2020-08-15 03:11:00 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,25 +5,17 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     index_page = Nokogiri::HTML(open(index_url))
-    info = index_page.css(".student-card").each do |student|
+    info = index_page.css(".student-card a").collect do |student|
     student_index = {
-      :name => index_page.css(".student_name")
-    } 
+      :name => student.css(".student-name").text,
+        :location => student.css(".student-location").text,
+        :profile_url => student.attr('href')
+    }
+    end
   end
-  
-  # projects = {}
-  # kickstarter.css("li.project.grid_4").each do |project|
-  #   title = project.css("h2.bbcard_name strong a").text
-  #   projects[title.to_sym] = {
-  #     :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
-  #     :description => project.css("p.bbcard_blurb").text,
-  #     :location => project.css("ul.project-meta span.location-name").text,
-  #     :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
-  #   }
-  # end
-
+ 
   def self.scrape_profile_page(profile_url)
-    student_profile = {}
+    student = {}
     profile_page = Nokogiri::HTML(open(profile_url))
     links = profile_page.css(".social-icon-container").children.css("a").map {|l| l.attribute("href").value}
     links.each do |link|
@@ -39,7 +31,7 @@ class Scraper
     end
     student[:profile_quote] = profile_page.css(".profile-quote").text if profile_page.css(".profile-quote")
     student[:bio] = profile_page.css("div.bio-content.content-holder div.description-holder p").text if profile_page.css("div.bio-content.content-holder div.description-holder p")
-  student_profile  
+  student 
   end
 
 end
