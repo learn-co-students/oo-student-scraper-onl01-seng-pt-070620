8dc3b26bbd0245d1f928d379d598fc10f8350664
github username <github email address>
2020-08-16 16:36:25 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -21,11 +21,12 @@ class Student
     self.send("twitter=", attributes_hash[:twitter])
     self.send("bio=", attributes_hash[:bio])
     self.send("blog=", attributes_hash[:blog])
-    
+    self.send("linkedin=", attributes_hash[:linkedin])
+    self.send("profile_quote=", attributes_hash[:profile_quote])
   end
 
   def self.all
-    
+    @@all
   end
 end
 
