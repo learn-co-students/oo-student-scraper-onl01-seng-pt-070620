6120cf12d39e83f71ddb3e06fba964c1a4ae5111
asjames18 <asjames18@gmail.com>
2020-08-15 20:40:27 -0400

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,31 +1,42 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    student = []
-
     doc = Nokogiri::HTML(open(index_url))
-
-    doc.css("div.roster-cards-container").each do |roster|
-      roster.css("div.student-card").each do |card|
-          name= card.css("h4.student-name").text
-          location= card.css("p.student-location").text
-          profile_url= card.xpath('//a/@href').text
-          # profile_url.each do  |attr| attr.value
-
-          # profile_url= card.css
-          student << {name:name, location:location, profile_url:profile_url}
-          binding.pry
+    students = []
+    doc.css("div.roster-cards-container").each do |card|
+      card.css(".student-card a").each do |student|
+        name = student.css(".student-name").text
+        location = student.css(".student-location").text
+        profile_url = "#{student.attr("href")}"
+        students << {name: name, location: location, profile_url: profile_url}
       end
     end
-
+    students
   end
 
   def self.scrape_profile_page(profile_url)
+    doc = Nokogiri::HTML(open(profile_url))
+     student = {}
 
+    social_icons = doc.css("div.social-icon-container a").collect {|x| x.attribute("href").value}
+    social_icons.each do |social_icon|
+      if social_icon.include?("linkedin")
+        student[:linkedin] = social_icon
+      elsif social_icon.include?("github")
+        student[:github] = social_icon
+      elsif social_icon.include?("twitter")
+        student[:twitter] = social_icon
+      else
+        student[:blog] = social_icon
+      end
+    end
+      student[:bio] = doc.css("div.bio-content.content-holder div.description-holder p").text
+    
+      student[:profile_quote] = doc.css(".profile-quote").text
+    student
   end
-
-end
-
+end	
